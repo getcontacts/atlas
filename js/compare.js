8d18e37c757d5dcafcbb6f8ef5718327d6d54b98
Rasmus Fonseca <fonseca.rasmus@gmail.com>
2018-05-09 17:18:46 -0700

    Edges almost computed

--- js/compare.js
+++ js/compare.js
@@ -17,16 +17,49 @@ function buildMultiFlareGraph(contacts, labels){
   };
   ret.trees.push(tree);
 
+  const n = contacts.length;
+
   // Build tree
   const paths = new Set();
-  labels.forEach(function(labelList){
-    labelList.forEach(function(label){
-      if (!paths.has(label[1])){
-        paths.add(label[1]);
-        tree.treeProperties.push({path: label[1]})
+  labels.forEach(function(labelDict){
+    for (const labelKey in labelDict){
+      const labelVal = labelDict[labelKey][0];
+      if (!paths.has(labelVal)){
+        paths.add(labelVal);
+        tree.treeProperties.push({path: labelVal})
       }
-    })
+    }
   });
+
+  // Helper function to find and create edges by [name1, name2] identifier
+  function findEdge(n1, n2){
+    if (n2 < n1){
+      // Swap
+      const tmpN = n2;
+      n2 = n1;
+      n1 = tmpN;
+    }
+
+    for (let e = 0; e < ret.edges.length; e += 1){
+      if (ret.edges[e].name1 == n1 && ret.edges[e].name2 == n2){
+        return ret.edges[e];
+      }
+    }
+    const edge = {name1: n1, name2: n2, frames: []};
+    ret.edges.push(edge);
+    return edge;
+  }
+
+  for (let i=0; i < n; i += 1){
+    contacts[i].forEach(function(contact){
+      const resi1 = contact[2].substr(0,contact[2].lastIndexOf(":"));
+      const resi2 = contact[3].substr(0,contact[3].lastIndexOf(":"));
+      const n1 = labels[i][resi1];
+      const n2 = labels[i][resi2];
+      const edge = findEdge(n1, n2);
+      edge.frames.push(i);
+    });
+  }
 
 
   return ret;
@@ -54,7 +87,8 @@ export function update(contactFiles, labelFiles, itypes) {
       labelsData = labelsData.map(function(ld){
         return ld.split("\n")
           .map((line) => line.split("\t"))
-          .filter((row) => row.length >= 2);
+          .filter((row) => row.length >= 2)
+          .reduce((acc, row) => {acc[row[0]] = row.slice(1); return acc;}, {});
       });
 
       const graph = buildMultiFlareGraph(contactsData, labelsData);
